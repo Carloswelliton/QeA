4668b219071747c1b63942c2c216b09a61567521
Carlos Welliton <139694187+Carloswelliton@users.noreply.github.com>
2025-05-12 11:52:02 -0400

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize("guiaperguntas", "root", "123456", {
+const connection = new Sequelize("mysql://root:iwYVdUTmBsthdIrHlapMVcrpPBvEKeWv@mainline.proxy.rlwy.net:25785/railway", {
     host: "localhost",
     dialect: "mysql"
 });
